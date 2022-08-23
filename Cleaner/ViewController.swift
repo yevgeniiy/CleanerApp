2e4af242ad8c7ac85f974d3ec3d6cc5c596a52b4
Yevgenii Kryzhanivskyi <ev.kryzhanivskyi@gmail.com>
2022-08-23 23:44:00 +0100

    Initial Commit

---
+++ Cleaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleaner
+//
+//  Created by Yevgenii Kryzhanivskyi on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
